3a95f04fb09b37c7c92ac100d3262feff6cf5dd6
Nat <natnmiller@gmail.com>
2018-11-05 11:47:17 -0800

    Lots of Comments and Break Down
    Here is the program with all of the comments I have so far.

--- Project 4/src/ChangeMaker.java
+++ Project 4/src/ChangeMaker.java
@@ -13,14 +13,27 @@ public class ChangeMaker {
 
     }
 
+    /* change_DPmmethod should implement a dynamic programming 
+    algorithm(Bottom-up method)*/
+    //receives the n amount and the d-array
     public static int[] change_DP(int n, int []d){
     	// needs the n amount and the d-array of coin values
     	// will be bottom up method
-        int [] C = new int[];
+
+    	//array C[0...n] to hold cj vals
+    	//goal get C[n]
+        int [] C = new int[n-1];
         int [] A = new int[];
 
 
         return C;
+        /*returns an array containing the count of coins for 
+        each di-denomination to be used when making change for the given 
+        n amount*/
+    }
+
+    private static void findOptiamalSol(){
+    	
     }
 
 }
