59d3f2fe8dc44cf5cca30a8e0458fb6b12369449
Stephanie <stephcarpin710@gmail.com>
2018-11-28 20:02:12 -0800

    all new updates (need help)

--- Project5/DiGraph.java
+++ Project5/DiGraph.java
@@ -8,110 +8,114 @@
 // ----https://www.geeksforgeeks.org/graph-and-its-representations/
 // Good Example: http://www.cs.cornell.edu/courses/cs211/2006fa/Lectures/L22-More%20Graphs/Digraph.java
 
-import java.util.Scanner; //not sure if we do need the scanner only in main
 import java.util.LinkedList;
 import java.io.*;
 
-//a directed graph as an array of Adjacency Linked Lists. 
+//a directed graph as an array of Adjacency Linked Lists.
 public class DiGraph {
-	// not sure if we can have this
-	int N;
-
-	//One private instance variable: this is an array of linked lists (use Java’s LinkedListclass).
-	private LinkedList<Integer>[] vertex;
-
-
-	// A constructor with one int type parameter for N. This constructor creates and
-	// initializes the instance variable-array
-
-
-	new DiGraph(int N) {
-		vertex = new LinkedList[N - 1];
-
-		// Create a new list for each vertex 
-		// such that adjacent nodes can be stored
-		for (int i = 0; i < N; i++) {
-			vertex[i] = new LinkedList<>();
-		}
-	}
-
-	//two parameters identify vertices representing the edge that needs to be added to the graph
-	// (to vertex is added as from vertex’s neighbor).
-	public addEdge(int from, int to) {
-
-		//the edge should not be added if it already exists: needs to be checked before adding
-		//if already exislts - do not add
-		if () {
-			vertex[src].add(dest);
-			vertex[from].add(to);
-		}
-
-
-		//vertex-numbers are given in natural numbering(starting with 1) so you should “turn
-		// ”them to Java-indexing to reflect correct connection. No need for validity check
-	}
-
-	//two parameters identify vertices representing the edge that needs to be deleted from the graph
-	// (to vertex is removed from vertex’s neighbor).
-	public deleteEdge(int from, int to) {
-		//nothing done if edge does not exist (no error message)
-
-
-		//vertex-numbers are given in natural numbering(starting with 1) so you should “turn
-		// ”them to Java-indexing to reflect correct connection. No need for validity check
-
-	}
-
-	//https://www.geeksforgeeks.org/count-number-edges-undirected-graph/
-	// computes and returns edges of graph
-	public int edgeCount() {
-		int edge_num = 0;
-
-		for (int i = 0; i < V; i++) {
-			sum += vertex[i].size;
-		}
-		// since it will transverse each edge twice
-		return edge_num / 2;
-	}
-
-
-	// returns number of vetices (its the arrays length)
-	public int vertexCount() {
-		int num_vert;
-
-		return num_vert;
-	}
-
-	//outputs the graph in the format provide din handout
-	public print() {
-		int num_vertix;
-
-		for (int i = 0; i <= num_vertix; i++) {
-			System.out.println((i + 1) + " is connected to: ");
-			//outputs a line: i is connected to: x1, ..., xk
-			//where x1,..., xk are vertices that are adjacent to i.
-		}
-	}
-
-// ---- PART 2 ------
-	// CHECK THIS OUT
-	//https://www.geeksforgeeks.org/topological-sorting/
-
-//include the implementation of the Topological Sort
-//algorithm including a supporting routine for computing vertex indegrees.
-
-	//returns an array of integers representing the indegrees of all vertices in the graph
-	//the i-th integer in the resulting array is the indegree of the i-th vertex.
-	private int[] indegrees() {
-
-	}
-
-	//returns an array containing the list of topologically sorted vertices
-	// (values in the array should represent natural vertex-numbers, i.e. starting with 1).
-	public int[] topSort() {
-
-		//If the graph is cyclic, this method must throw IllegalArgumentException type exception
-		//(read the note on top of the last page of your Topological Sort lecture handout).
-
-	}
+    int N;
+    //*** --- HOW/WHERE TO SAVE THIS N IF IT IN IN CONSTRUCTOR ---****
+    
+    //One private instance variable: this is an array of linked lists (use Java’s LinkedListclass).
+    private LinkedList<Integer>[] arr;
+    
+    // A constructor with one int type parameter for N. This constructor creates and
+    // initializes the instance variable-array
+    
+    DiGraph(int N) {
+        arr = (LinkedList<Integer>[]) new LinkedList[N];
+        
+        // Create a new list for each vertex
+        // such that adjacent nodes can be stored
+        for (int i = 0; i < N; i++) {
+            arr[i] = new LinkedList<>();
+        }
+    }
+    
+    //two parameters identify vertices representing the edge that needs to be added to the graph
+    // (to vertex is added as from vertex’s neighbor).
+    public void addEdge(int from, int to) {
+        //the edge should not be added if it already exists
+        if (!arr[from].contains(to)) {
+            arr[from].add(to);
+        }
+        
+        //*** if it does exist do we output error message ****
+        
+        //vertex-numbers are given in natural numbering(starting with 1) so you should “turn
+        // ”them to Java-indexing to reflect correct connection. No need for validity check
+    }
+    
+    //two parameters identify vertices representing the edge that needs to be deleted from the graph
+    // (to vertex is removed from vertex’s neighbor).
+    public void deleteEdge(int from, int to) {
+        //nothing done if edge does not exist (no error message)
+        
+        //*** DO WE NEED THIS IF STATEMENT IF WE JUST DELETE? ****
+        if(arr[from].contains(to)){
+            arr[from].remove(to);
+        }
+    }
+    
+    //vertex-numbers are given in natural numbering(starting with 1) so you should “turn
+    // ”them to Java-indexing to reflect correct connection. No need for validity check
+    
+    
+    //https://www.geeksforgeeks.org/count-number-edges-undirected-graph/
+    // computes and returns edges of graph
+    public int edgeCount() {
+        int edge_num = 0;
+        
+        for (int i = 0; i < N; i++) {
+            edge_num += arr[i].size();
+        }
+        // since it will transverse each edge twice
+        return edge_num / 2;
+    }
+    
+    
+    // returns number of vetices (its the arrays length)
+    public int vertexCount() {
+        return N;
+    }
+    
+    //outputs the graph in the format provide din handout
+    public void print() {
+        
+        for (int i = 0; i < N; i++) {
+            System.out.print((i + 1) + " is connected to: ");
+            for(int j = 0; j < arr[i].size(); j++) {
+                System.out.print(arr[i].get(j));
+                if(arr[i].size() > 1 && j != arr[i].size()) {
+                    System.out.print(",");
+                }
+                //outputs a line: i is connected to: x1, ..., xk
+                //where x1,..., xk are vertices that are adjacent to i.
+            }
+        }
+    }
+    
+    
+    // ******---- PART 2 ------******
+    // CHECK THIS OUT
+    //https://www.geeksforgeeks.org/topological-sorting/
+    
+    //include the implementation of the Topological Sort
+    //algorithm including a supporting routine for computing vertex indegrees.
+    
+    //returns an array of integers representing the indegrees of all vertices in the graph
+    //the i-th integer in the resulting array is the indegree of the i-th vertex.
+    /*private int[] indegrees() {
+     
+     }
+     
+     //returns an array containing the list of topologically sorted vertices
+     // (values in the array should represent natural vertex-numbers, i.e. starting with 1).
+     public int[] topSort() {
+     
+     //If the graph is cyclic, this method must throw IllegalArgumentException type exception
+     //(read the note on top of the last page of your Topological Sort lecture handout).
+     
+     }*/
 }
+
