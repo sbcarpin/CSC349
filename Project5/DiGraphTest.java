5e9a6fafe49a6a3870e656b4695a7b4ff3c47b44
Stephanie <stephcarpin710@gmail.com>
2018-11-29 23:36:51 -0800

    changes to main

--- Project5/DiGraphTest.java
+++ Project5/DiGraphTest.java
@@ -9,16 +9,16 @@ import java.util.Scanner;
 import java.io.*;
 
 public class DiGraphTest {
-
+    
     public static void main(String[] args) {
         Scanner keyboard = new Scanner(System.in);
-
+        
         System.out.println("Enter number of vertices: ");
         int num_vert = keyboard.nextInt();
         //input the number of vertices and define an object of DiGraph class.
-
+        
         DiGraph diobject = new DiGraph(num_vert);
-
+        
         System.out.println("Choose one of the following operations: ");
         System.out.println("- add edge (enter a)");
         System.out.println("- delete edge (enter d)");
@@ -28,26 +28,22 @@ public class DiGraphTest {
         System.out.println("- Topological sort (enter t)"); // PART 2
         System.out.println("- Quit (enter q)");
         int num1, num2;
-        char input;
-
+        
         //as long as the user does not enter "q" for quit keeo doing the below
         while(true) {
+            char input = keyboard.next().charAt(0);
             String i = keyboard.nextLine();
-            input = 0;
-
-            if(i.length() == 1){
-                input = i.charAt(0);
+            //char input = 'x';
+            
+            System.out.println("String length: " + i.length());
+            if(i.length() != 0){
+                input = 'x'; // to go to default
             }
-
-
-            if(input == 'q'){
-                System.out.println("Quiting... Goodbye.");
-                System.exit(0);
-            }
-
-
-
+            
             switch(input){
+                case 'q':
+                    System.out.println("Quiting... Goodbye.");
+                    System.exit(0);
                 case 'a':
                     System.out.println("Enter two numbers please: ");
                     num1 = keyboard.nextInt();
@@ -76,14 +72,13 @@ public class DiGraphTest {
                     break;
                 case 't':
                     System.out.println("The graph is the following: ");
-                    diobject.topSort();
+                    //diobject.topSort();
                     break;
                 default:
                     System.out.println("Invalid menu choice. Please try again.");
                     break;
             }
             System.out.println("Choose one of the operations: ");
-
         }
     }
 }
