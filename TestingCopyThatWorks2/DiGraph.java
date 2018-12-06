5a21f443cfa843dc1b7f0ebef74a1ec7241cc3ba
Nat <natnmiller@gmail.com>
2018-12-05 20:55:23 -0800

    PUSH

--- TestingCopyThatWorks2/DiGraph.java
+++ TestingCopyThatWorks2/DiGraph.java
@@ -5,22 +5,20 @@ import java.util.Queue;
 public class DiGraph {
 
     private class VertexInfo {
-        
-        // natural indexing
+
         private int distance;
-        private int predecessor;
-        
-        public VertexInfo(int distance, int predecessor) {
+        private int pred;
+
+        public VertexInfo(int distance, int pred) {
             this.distance = distance;
-            this.predecessor = predecessor; 
+            this.pred = pred;
         }
     }
-    
-    
-    private LinkedList<Integer>[] linkedListArray;
-    
+
+    private LinkedList<Integer>[] arr;
+
     private VertexInfo[] BFS(int s) {
-        int N = linkedListArray.length;
+        int N = arr.length;
         VertexInfo[] VA = new VertexInfo[N+1];
         for (int u=1; u<=N; u++) {
             VA[u] = new VertexInfo(-1, -1);
@@ -30,36 +28,35 @@ public class DiGraph {
         queue.add(s);
         while (!queue.isEmpty()) {
             int u = (int) (queue.remove());
-            for (int v=0; v<linkedListArray[u-1].size(); v++) { // for u's each adjacent vertex v
-                int vertex = linkedListArray[u-1].get(v);
-                if (VA[vertex].distance == -1) { // v not discovered yet
-                    VA[vertex].distance = VA[u].distance + 1;
-                    VA[vertex].predecessor = u;
-                    queue.add(vertex);
-                }               
+            for (int v=0; v<arr[u-1].size(); v++) { 
+                int vert = arr[u-1].get(v);
+                if (VA[vert].distance == -1) {
+                    VA[vert].distance = VA[u].distance + 1;
+                    VA[vert].pred = u;
+                    queue.add(vert);
+                }
             }
 
         }
         return VA;
-    
+
     }
-    
-    
+
+
     public boolean isTherePath(int from, int to) {
         VertexInfo[] VA = BFS(from);
-        if (VA[to].predecessor == -1) {
+        if (VA[to].pred == -1) {
             return false;
         }
         return true;
-        
+
     }
-    
+
     public int lengthOfPath(int from, int to) {
-        //call BFS, use data from array
         VertexInfo[] VA = BFS(from);
         return VA[to].distance;
     }
-    
+
     public void printPath(int from, int to) {
         VertexInfo[] VA = BFS(from);
         if (VA[to].distance == -1) {
@@ -69,72 +66,72 @@ public class DiGraph {
             String output = "";
             while (from != to) {
                 output = " -> " + to + output;
-                to = VA[to].predecessor;
+                to = VA[to].pred;
             }
             output = from + output;
             System.out.println(output);
         }
     }
-    
+
     public DiGraph(int n) {
-        linkedListArray = new LinkedList[n];
+        arr = new LinkedList[n];
         for (int i = 0; i < n; i++) {
-            linkedListArray[i] = new LinkedList<Integer>();
+            arr[i] = new LinkedList<Integer>();
         }
     }
 
     public boolean addEdge(int from, int to) {
-        if (linkedListArray[from-1].contains(to)) {
+        if (arr[from-1].contains(to)) {
             return false;
         }
-        linkedListArray[from-1].add(to);
+        arr[from-1].add(to);
         return true;
     }
-    
+
     public void deleteEdge(int from, int to) {
-        int index = linkedListArray[from-1].indexOf(to);
-        linkedListArray[from-1].remove(index);
+        int index = arr[from-1].indexOf(to);
+        arr[from-1].remove(index);
     }
-    
+
     public int edgeCount() {
         int edgeSum = 0;
-        for (int i = 0; i < linkedListArray.length; i++) {
-            edgeSum += linkedListArray[i].size();
+        for (int i = 0; i < arr.length; i++) {
+            edgeSum += arr[i].size();
         }
         return edgeSum;
     }
-    
+
     public int vertexCount() {
-        return linkedListArray.length;
+        return arr.length;
     }
-    
+
     public void print() {
-        for (int i = 0; i < linkedListArray.length; i++) {
+        for (int i = 0; i < arr.length; i++) {
             System.out.print((i+1) + " is connected to: ");
-            for (int j = 0; j < linkedListArray[i].size(); j++) {
-                System.out.print(linkedListArray[i].get(j));
-                if (j != linkedListArray[i].size() - 1) {
+            for (int j = 0; j < arr[i].size(); j++) {
+                System.out.print(arr[i].get(j));
+                if (j != arr[i].size() - 1) {
                     System.out.print(", ");
                 }
             }
             System.out.println();
         }
     }
-    
+
     private int[] indegrees() {
-        int n = linkedListArray.length;
+        int n = arr.length;
         int[] indegrees = new int[n];
-        for (int u = 0; u < n; u++) { // loop through vertex array
-            for (int v = 0; v < linkedListArray[u].size(); v++) { // loop through each linked list
-                indegrees[linkedListArray[u].get(v) - 1] += 1;
+        for (int u = 0; u < n; u++) { 
+            for (int v = 0; v < arr[u].size(); v++) {
+                indegrees[arr[u].get(v) - 1] += 1;
             }
         }
 
         return indegrees;
     }
-    
+
     public int[] topSort() throws IllegalArgumentException {
-        int n = linkedListArray.length;
+        int n = arr.length;
         int []indegrees = indegrees();
         int[] a = new int[n];
         Queue<Integer> q = new LinkedList<Integer>();
@@ -148,32 +145,31 @@ public class DiGraph {
             int u = q.remove();
             a[i] = u;
             i++;
-            for (int v = 0; v < linkedListArray[u-1].size(); v++) {
-                indegrees[linkedListArray[u-1].get(v) - 1]--;
-                if (indegrees[linkedListArray[u-1].get(v) - 1] == 0) {
-                    q.add(linkedListArray[u-1].get(v));
+            for (int v = 0; v < arr[u-1].size(); v++) {
+                indegrees[arr[u-1].get(v) - 1]--;
+                if (indegrees[arr[u-1].get(v) - 1] == 0) {
+                    q.add(arr[u-1].get(v));
                 }
-            }   
+            }
         }
         if (i != n) {
             throw new IllegalArgumentException();
         }
         return a;
     }
-    
-    
+
+
     private class TreeNode {
-        
-        private int vertex;
-        private LinkedList<TreeNode> children;
-        
-        public TreeNode(int vertex, LinkedList<TreeNode> children) {
-            this.vertex = vertex;
-            this.children = children;
+
+        private int vert;
+        private LinkedList<TreeNode> child;
+
+        public TreeNode(int vert, LinkedList<TreeNode> child) {
+            this.vert = vert;
+            this.child = child;
         }
-        
     }
-    
+
     private TreeNode buildTree(int s) {
         VertexInfo[] VA = BFS(s);
         int N = VA.length - 1;
@@ -182,28 +178,28 @@ public class DiGraph {
             treeNodes[i] = new TreeNode(i, new LinkedList<TreeNode>());
         }
         for (int i=1; i<=N; i++) {
-            int predecessor = VA[i].predecessor;
-            if (predecessor != -1) {
-                treeNodes[predecessor].children.add(treeNodes[i]);
+            int pred = VA[i].pred;
+            if (pred != -1) {
+                treeNodes[pred].child.add(treeNodes[i]);
             }
         }
         return treeNodes[s];
     }
-    
-    
+
+
     public void printTree(int s) {
         TreeNode root = buildTree(s);
         String indent = "";
         printTreeRecursive(root, indent);
     }
-    
+
     private void printTreeRecursive(TreeNode root, String indent) {
-        System.out.println(indent + root.vertex + " ");
-        if (root.children.isEmpty()) {
+        System.out.println(indent + root.vert + " ");
+        if (root.child.isEmpty()) {
             return;
         }
-        for (int i=0; i<root.children.size(); i++) {
-            printTreeRecursive(root.children.get(i), indent + "    ");
-        }   
+        for (int i=0; i<root.child.size(); i++) {
+            printTreeRecursive(root.child.get(i), indent + "    ");
+        }
     }
 }
